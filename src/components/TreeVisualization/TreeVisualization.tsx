6c1b3d1046eba731454e34e64b84732141f58e2a
denis <INNOWISE@Mac-add-2481-10.local>
2024-06-24 17:23:04 +0400

    fix lint

--- src/components/TreeVisualization/TreeVisualization.tsx
+++ src/components/TreeVisualization/TreeVisualization.tsx
@@ -4,54 +4,57 @@ import './TreeVisualization.css';
 import { AddNewElementModal } from '../AddNewElementModal';
 
 interface TreeVisualizationProps {
-    node: TreeNode | null;
-    isExpanded?: boolean;
+  node: TreeNode | null;
+  isExpanded?: boolean;
 }
 
-export const TreeVisualization = ({ node, isExpanded = false }: TreeVisualizationProps) => {
-    const [expanded, setExpanded] = useState<boolean>(!!isExpanded);
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
-    if (!node ){
-        return null
-    }
+export const TreeVisualization = ({
+  node,
+  isExpanded = false,
+}: TreeVisualizationProps) => {
+  const [expanded, setExpanded] = useState<boolean>(!!isExpanded);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  if (!node) {
+    return null;
+  }
 
-    const toggleExpand = () => {
-        setExpanded(!expanded);
-    };
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
 
-    const handleCloseModal = () =>{
-        setIsModalOpen(false)
-    }
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
-    const handleOpenModal = ()=>{
-        setIsModalOpen(true)
-    }
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
 
-    return (
-        <>
-        <AddNewElementModal 
-            isOpen={isModalOpen}
-            onClose={handleCloseModal}
-            node={node}
-        />
-            <div >
-                <div className='value-container'>
-                <div className="node-value pointer" onClick={toggleExpand}>
-                    {!!node.children.length && (expanded  ? '[-]' : '[+]')} {node.displayValue}
-                </div>
-                {!node.displayValue.includes('.') &&
-                    <button onClick={handleOpenModal}>add new</button>
-                }
-                </div>
-                {expanded && node.children.length > 0 && (
-                    <div className="children">
-                        {node.children.map((child) => (
-                            <TreeVisualization key={child.value} node={child} />
-                        ))}
-                    </div>
-                )}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <AddNewElementModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        node={node}
+      />
+      <div>
+        <div className="value-container">
+          <div className="node-value pointer" onClick={toggleExpand}>
+            {!!node.children.length && (expanded ? '[-]' : '[+]')}{' '}
+            {node.displayValue}
+          </div>
+          {!node.displayValue.includes('.') && (
+            <button onClick={handleOpenModal}>add new</button>
+          )}
+        </div>
+        {expanded && node.children.length > 0 && (
+          <div className="children">
+            {node.children.map((child) => (
+              <TreeVisualization key={child.value} node={child} />
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
-
